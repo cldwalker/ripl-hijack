48eb6378b2c5a3b0a39b93067af617f1df5540b6
Gabriel Horner <gabriel.horner@gmail.com>
2010-12-15 00:55:57 -0500

    fix commands and clean up executable

--- lib/ripl/hijack.rb
+++ lib/ripl/hijack.rb
@@ -12,23 +12,20 @@ end
 
 module Ripl::Hijack
   def before_loop
+    Hijack.start(config[:hijack_pid], config[:hijack_options])
+    @binding = loop_eval("self").instance_eval { binding }
     super
-    add_commands loop_eval("self")
+  end
+
+  def after_loop
+   Hijack::Console.remote.evaluate('__hijack_exit') rescue nil
   end
 
   def loop_eval(str)
-    Hijack::Console.remote.evaluate(str)
-  end
-
-  module Commands
-    Hijack::Helper.methods.grep(/^hijack_/) do |meth|
-      define_method(meth) do
-        Hijack::Helper.send(meth, Hijack::Console.remote)
-      end
-    end
+    (helper = Hijack::Helper.find_helper(str)) ?
+      Hijack::Helper.send(helper, Hijack::Console.remote) :
+      Hijack::Console.remote.evaluate(str)
   end
 end
 
 Ripl::Shell.send :include, Ripl::Hijack
-Ripl::Commands.send :include, Ripl::Hijack::Commands
-p Ripl::Commands.instance_methods
